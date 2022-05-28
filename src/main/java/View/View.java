0bfd079741bbdd29e34170f8c2d62a8abce182d5
AlexeiK <korolev.av@phystech.edu>
2022-05-29 01:05:37 +0300

    Added resources + view fix

--- src/main/java/View/View.java
+++ src/main/java/View/View.java
@@ -1,9 +1,32 @@
 package main.java.View;
 
 import javax.swing.*;
+import java.awt.*;
 
 public class View extends JFrame {
+    private JPanel panel;
+
     public static void main(String[] args) {
-        new View().setVisible(true);
+        new View();
+    }
+
+    private View (){
+        initPanel();
+        initFrame();
+    }
+
+    private void initPanel () {
+        panel = new JPanel();
+        panel.setPreferredSize(new Dimension(300, 300));
+        add(panel);
+    }
+
+    private void initFrame () {
+        pack();
+        setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        setTitle("Weather");
+        setLocationRelativeTo(null);
+        setResizable(false);
+        setVisible(true);
     }
 }
