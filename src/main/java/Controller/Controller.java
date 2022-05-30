a95f7c77b87f0ef6de548b2c5e7867a0f02de238
AlexeiK <korolev.av@phystech.edu>
2022-05-30 08:26:34 +0300

    Added weather text output

--- src/main/java/Controller/Controller.java
+++ src/main/java/Controller/Controller.java
@@ -3,10 +3,13 @@ import Model.Model;
 import View.View;
 import org.json.JSONException;
 
+import javax.print.attribute.standard.JobKOctets;
 import javax.swing.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.Objects;
 
 public class Controller implements ActionListener {
@@ -16,13 +19,34 @@ public class Controller implements ActionListener {
     private JLabel startWarn;
     private int revert;
 
-    public Controller(JButton next, JButton prev, JTextField tf, JLabel startWarn){
+    public JLabel timeStamp;
+    public JLabel characteristic;
+    public JLabel temperature;
+    public JLabel humidity;
+    public JLabel feelsLike;
+    public JLabel pressure;
+    public JLabel wind;
+    public JLabel clouds;
+    public JLabel place;
+
+    public Controller(JButton next, JButton prev, JTextField tf, JLabel startWarn, JLabel timeStamp, JLabel characteristic,
+                      JLabel temperature, JLabel feelsLike, JLabel pressure, JLabel wind, JLabel clouds, JLabel humidity, JLabel place){
         this.next = next;
         this.prev = prev;
         this.tf = tf;
         this.startWarn = startWarn;
         this.revert = 0;
+        this.timeStamp = timeStamp;
+        this.characteristic = characteristic;
+        this.temperature = temperature;
+        this.humidity = humidity;
+        this.feelsLike = feelsLike;
+        this.pressure = pressure;
+        this.wind = wind;
+        this.clouds = clouds;
+        this.place = place;
     }
+
 
     @Override
     public void actionPerformed(ActionEvent e) {
@@ -34,12 +58,16 @@ public class Controller implements ActionListener {
                 revert--;
                 model = new Model(revert);
                 if (model.time == 1) {
+                    hide();
                     startWarn.setText("This city was last. Press >> to go forth");
                 } else {
-                    startWarn.setText("City is " + model.name + " " + Integer.toString(revert) + ";\n");
+                    printText(model);
+                    //startWarn.setText("City is " + model.name + " " + Integer.toString(revert) + ";\n");
+                    startWarn.setText("");
                 }
             } catch (ArrayIndexOutOfBoundsException | IOException | JSONException ex) {
                 revert++;
+                hide();
                 startWarn.setText("You saw all the forecasts.");
                 throw new RuntimeException(ex);
             }
@@ -53,8 +81,11 @@ public class Controller implements ActionListener {
                     if (!Objects.equals(cityName, "City name...")) {
                         // System.out.println(cityName);
                         model = new Model(cityName);
+                        printText(model);
+                        startWarn.setText("");
                     } else {
                         // revert++;
+                        hide();
                         startWarn.setText("Please, insert desired city in the text field.");
                     }
                 } else {
@@ -62,15 +93,52 @@ public class Controller implements ActionListener {
                     model = new Model(revert);
                     System.out.print(model.time);
                     if (model.time == 2000000000) {
+                        hide();
                         startWarn.setText("Insert your city and press >>!");
                     } else {
-                        startWarn.setText("City is " + model.name + " " + Integer.toString(revert) + ";\n");
+                        printText(model);
+                        //startWarn.setText("City is " + model.name + " " + Integer.toString(revert) + ";\n");
+                        startWarn.setText("");
                     }
                 }
             } catch (IOException ex) {
+                hide();
                 startWarn.setText("Entered city was not found in base");
                 throw new RuntimeException(ex);
             }
         }
+    }
+
+    private void hide() {
+        timeStamp.setText("");
+        characteristic.setText("");
+        temperature.setText("");
+        humidity.setText("");
+        feelsLike.setText("");
+        pressure.setText("");
+        wind.setText("");
+        clouds.setText("");
+        place.setText("");
+    }
+
+    private void printText(Model model) {
+        int seco = model.time;
+        Date date = new java.util.Date(seco * 1000L);
+        SimpleDateFormat sdf = new java.text.SimpleDateFormat("z HH:mm:ss dd.MM.yyyy");
+        sdf.setTimeZone(java.util.TimeZone.getTimeZone("GMT+3"));
+        String formattedDate = sdf.format(date);
+
+        //if (model.windDeg) {
+
+        //}
+        place.setText("Place: " + model.name);
+        timeStamp.setText(formattedDate);
+        characteristic.setText(model.desc);
+        temperature.setText(Integer.toString(model.temp) + "°C");
+        humidity.setText("Humidity: " + Integer.toString(model.humidity) + "%");
+        feelsLike.setText("Feels like: " + Integer.toString(model.feelsLike) + "°C");
+        pressure.setText("Pressure: " + Integer.toString(model.pressure * 3 / 4) + " mmHg");
+        wind.setText("Wind: " + Float.toString(model.windSpeed) + " m/sec"); // + Integer.toString(model.windDeg) + ", "
+        clouds.setText("Clouds are filling " + Integer.toString(model.clouds) + "% of sky");
     }
 }
--- src/main/java/View/View.java
+++ src/main/java/View/View.java
@@ -14,10 +14,20 @@ public class View extends JFrame {
     public JButton prev;
     public JTextField tf;
     public JLabel startWarn;
+    public JLabel timeStamp;
+    public JLabel characteristic;
+    public JLabel temperature;
+    public JLabel humidity;
+    public JLabel feelsLike;
+    public JLabel pressure;
+    public JLabel wind;
+    public JLabel clouds;
+    public JLabel place;
 
     public static void main(String[] args) throws IOException {
         new View();
     }
+
 
     public View() throws IOException {
         frame = new JFrame("Weather");
@@ -25,20 +35,68 @@ public class View extends JFrame {
         prev = new JButton("<<");
         tf = new JTextField("City name...");
         startWarn = new JLabel("Type your city and press \">>\"...");
+        timeStamp = new JLabel("");
+        temperature = new JLabel("");
+        feelsLike = new JLabel("");
+        characteristic = new JLabel("");
+        humidity = new JLabel("");
+        pressure = new JLabel("");
+        wind = new JLabel("");
+        clouds = new JLabel("");
+        place = new JLabel("");
+        timeStamp.setBounds(0, 0, 280, 20);
+        place.setBounds(0, 0, 280, 60);
+        characteristic.setBounds(0, 120, 280, 30);
+        temperature.setBounds(120, 30, 140, 80);
+        feelsLike.setBounds(0, 100, 280, 30);
+        pressure.setBounds(0, 140, 280, 30);
+        humidity.setBounds(0, 160, 280, 30);
+        wind.setBounds(0, 180, 280, 30);
+        clouds.setBounds(0, 200, 280, 30);
+        timeStamp.setHorizontalAlignment(JTextField.CENTER);
+        timeStamp.setVerticalAlignment(JTextField.CENTER);
+        temperature.setHorizontalAlignment(JTextField.CENTER);
+        temperature.setVerticalAlignment(JTextField.CENTER);
+        feelsLike.setHorizontalAlignment(JTextField.CENTER);
+        feelsLike.setVerticalAlignment(JTextField.CENTER);
+        pressure.setHorizontalAlignment(JTextField.CENTER);
+        pressure.setVerticalAlignment(JTextField.CENTER);
+        wind.setHorizontalAlignment(JTextField.CENTER);
+        wind.setVerticalAlignment(JTextField.CENTER);
+        clouds.setHorizontalAlignment(JTextField.CENTER);
+        clouds.setVerticalAlignment(JTextField.CENTER);
+        humidity.setHorizontalAlignment(JTextField.CENTER);
+        humidity.setVerticalAlignment(JTextField.CENTER);
+        characteristic.setHorizontalAlignment(JTextField.CENTER);
+        characteristic.setVerticalAlignment(JTextField.CENTER);
+        place.setHorizontalAlignment(JTextField.CENTER);
+        place.setVerticalAlignment(JTextField.CENTER);
+
         startWarn.setBounds(0, 0, 280, 260);
         startWarn.setHorizontalAlignment(JTextField.CENTER);
         startWarn.setVerticalAlignment(JTextField.CENTER);
         next.setBounds(220, 230, 60, 30);
-        ActionListener actionListener = new Controller(next, prev, tf, startWarn);
+        ActionListener actionListener = new Controller(next, prev, tf, startWarn, timeStamp,  characteristic, temperature, feelsLike, pressure, wind,
+                clouds, humidity, place);
         next.addActionListener(actionListener);
         prev.addActionListener(actionListener);
         tf.setBounds(70, 230, 150, 30);
         prev.setBounds(7, 230, 60, 30);
         frame.add(startWarn);
+        frame.add(timeStamp);
+        frame.add(temperature);
+        frame.add(feelsLike);
+        frame.add(pressure);
+        frame.add(humidity);
+        frame.add(wind);
+        frame.add(clouds);
+        frame.add(characteristic);
         frame.add(tf);
         frame.add(next);
         frame.add(prev);
+        frame.add(place);
 
+        temperature.setFont(new Font("Serif", Font.PLAIN, 64));
         frame.setSize(300, 300);
         frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
         frame.setLayout(null);
